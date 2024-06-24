a0f50cdc7c03ae5485b1dec60ba37c7961f7b9b6
Harish <133589643+HarishRajaK@users.noreply.github.com>
2024-06-24 12:20:28 +0530

    Create Strassen’s Matrix Multiplication.c

---
+++ Strassen’s Matrix Multiplication.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+void addMatrix(int a[2][2], int b[2][2], int result[2][2]) {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            result[i][j] = a[i][j] + b[i][j];
+        }
+    }
+}
+
+void subMatrix(int a[2][2], int b[2][2], int result[2][2]) {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            result[i][j] = a[i][j] - b[i][j];
+        }
+    }
+}
+
+void strassen(int a[2][2], int b[2][2], int result[2][2]) {
+    int m1, m2, m3, m4, m5, m6, m7;
+    int temp1[2][2], temp2[2][2];
+
+    m1 = (a[0][0] + a[1][1]) * (b[0][0] + b[1][1]);
+    m2 = (a[1][0] + a[1][1]) * b[0][0];
+    m3 = a[0][0] * (b[0][1] - b[1][1]);
+    m4 = a[1][1] * (b[1][0] - b[0][0]);
+    m5 = (a[0][0] + a[0][1]) * b[1][1];
+    m6 = (a[1][0] - a[0][0]) * (b[0][0] + b[0][1]);
+    m7 = (a[0][1] - a[1][1]) * (b[1][0] + b[1][1]);
+
+    result[0][0] = m1 + m4 - m5 + m7;
+    result[0][1] = m3 + m5;
+    result[1][0] = m2 + m4;
+    result[1][1] = m1 - m2 + m3 + m6;
+}
+
+int main() {
+    int a[2][2], b[2][2], result[2][2];
+
+    printf("Enter elements of first matrix (2x2):\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of second matrix (2x2):\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    strassen(a, b, result);
+
+    printf("Resultant matrix:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
